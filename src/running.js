40472d801f5f52c50782e62be4dd0893efc6e26e
yomisana <sammywhy3236@gmail.com>
2023-04-24 02:27:20 +0800

    Just worng event
    This reverts commit fad6045d7dc08134ab53fcb0df0343601939ed32.

--- src/running.js
+++ src/running.js
@@ -1,4 +1,5 @@
 // 測試的參數在 Windows 11 pro 剛裝下去
+
 // @echo off
 
 // set PYTHON=C:\Users\Yomisana\Desktop\bin\stable-diffusion-webui\venv\Scripts\python.exe
@@ -9,20 +10,36 @@
 // call webui.bat
 
 const $ = {
-    read_sd_config:function(){
-        let filePath = `${app_location.file.command_args}`;
-        if (!fs.existsSync(filePath)) {
-            // console.log('文件不存在');
-            return `nofile`;
-        }
-        let fileContent = fs.readFileSync(filePath, 'utf-8');
-        if (fileContent.trim().length === 0) {
-            // console.log('文件內容為空');
-            return false;
-        } else {
-            // console.log('文件內容為:', fileContent);
-            return fileContent;
-        }
+    main_last: async function(){
+        $.vcr();
+        $.sd();
+        // 如果找不到 $.basic_settings_sd(); 所輸出的 config/command_args.txt 就跳轉到 $.basic_settings_sd();
+        pressAnyKey(`${i.__('pressAnyKey')}`, {
+            ctrlC: "reject"
+        }).then(async () => {
+            cmd.clear(); await ascii_art("red", app_name);        menu.status();
+        }).catch(() => {
+            console.log('You pressed CTRL+C');        menu.status();
+        })
+    },
+    main: async function(){
+        $.vcr();
+        $.sd();
+        $.basic_settings_sd();
+        pressAnyKey(`${i.__('pressAnyKey')}`, {
+            ctrlC: "reject"
+        }).then(async () => {
+            cmd.clear(); await ascii_art("red", app_name);        menu.status();
+        }).catch(() => {
+            console.log('You pressed CTRL+C');        menu.status();
+        })
+    },
+    basic_settings_sd: function(){
+
+    },
+    sd: function(){
+        lastrunstatus = true;
+        config.set("lastrunstatus", lastrunstatus);
     },
     vcr: function(){
         // // 檢查是否有 VC Redist 的註冊表項目存在
@@ -46,102 +63,15 @@ const $ = {
                     console.error(`stderr: ${stderr}`);
                 });
             }else{
-                // if(result){
-                //     console.log(color("green"), `${version} VC Redist - Installed`)
-                // }else{
-                //     console.log(color("red"), `${version} VC Redist - Not Install`);
-                // }
+                if(result){
+                    console.log(color("green"), `${version} VC Redist - Installed`)
+                }else{
+                    console.log(color("red"), `${version} VC Redist - Not Install`);
+                }
             }
         });
         console.log("Finished VC Redist check & install")
-    },
-    exit: function(){
-        pressAnyKey(`${i.__('pressAnyKey')}`, {
-            ctrlC: "reject"
-        }).then(async () => {
-            cmd.clear(); await ascii_art("red", app_name);        menu.status();
-        }).catch(() => {
-            console.log('You pressed CTRL+C');        menu.status();
-        })
-    },
-    // 上方為已完成不要在動他了
-    // 下方未完成
-    main: async function(){
-        let re = $.read_sd_config();
-        if(re === `nofile` || re === false){
-            console.log("no cmdline_args file/ cmdline_args file not have content")
-            console.log("create new one of cmdline_args.");
-            $.basic_settings_sd();
-        }else{
-            console.log("設定檔裡面已經有先前所記錄的設定參數，你是否要繼續設定新參數或是使用上一次狀態執行Stable Diffusion，如果繼續將會覆蓋舊有的參數數據!")
-            inquirer.prompt([
-                {
-                type: 'list',
-                name: 'choice',
-                message: `${i.__('Please choose what you wanna do?')}:`,
-                choices: [
-                    `${i.__('Run as last status')}`,
-                    `${i.__('keep create new one')}`,
-                    `${i.__('Cancel back menu')}`,
-                ]
-                }
-            ]).then(function(answers) {
-                switch (answers.choice) {
-                    case `${i.__('Run as last status')}`:
-                        $.main_last();
-                        break;
-                    case `${i.__('keep create new one')}`:
-                        $.basic_settings_sd();
-                        break;
-                    case `${i.__('Cancel back menu')}`:
-                        menu.status();
-                        break;
-                    default:
-                        break;
-                }
-            });
-        }
-        // console.log("開始準備 Stable Diffusion....");
-        // $.prepare();
-        // $.exit();
-    },
-    basic_settings_sd: async function(){
-        gpulist = [];
-        console.log("硬體掃描顯示卡中...");
-        await check.gpu().then((value) => {
-            console.log(color("yellow"),`[GPU  X] Find ${value.length} GPU(s)`);
-            let data = value.map((x, index) => {
-                gpulist.push(`${x.model} ${x.vram} GB`)
-            });
-        });
-        console.log(gpulist);
-        console.log(`僅供參考: 你的記憶體小於 8G 記憶體`)
-        console.log(`如果你小於 6G 必定確定新增低顯存指令(6G版本的顯卡可能需要或是不需要)`)
-        console.log(`你的顯卡好像僅支援[半精度(FP16),單精度(FP32),雙精度(FP64)]`);
-        console.log(`可能引發 某些模型使用 半精度(FP16) 製作的可能會無法使用`);
-        console.log(`可能引發 某些模型使用 單精度(FP32) 製作的可能會無法使用`);
-        console.log(`可能引發 某些模型使用 雙精度(FP64) 製作的可能會無法使用`);
-    },
-    prepare: function(){
-        $.vcr();
-        $.sd();
-    },
-    sd: function(){
-        lastrunstatus = true;
-        config.set("lastrunstatus", lastrunstatus);
-    },
-    main_last: async function(){
-        console.log("讀取最後一次的設定檔中....");
-        // try to read last config/command_args.txt is have commandline or file is exists?
-        let re = $.read_sd_config();
-        console.log(re);
-        if(re === false){
-            await $.basic_settings_sd();
-        }
-        $.prepare();
-        // 如果找不到 $.basic_settings_sd(); 所輸出的 config/command_args.txt 就跳轉到 $.basic_settings_sd();
-        $.exit();
-    },
+    }
 }
 
 module.exports = $;
